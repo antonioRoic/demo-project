57a58b2ab76e6164816739cc11a69ac3eebe49e2
Antonio <antonioroic@gmail.com>
2018-10-11 12:30:26 +0200

    My second commit

--- src/App.js
+++ src/App.js
@@ -14,45 +14,60 @@ class App extends Component {
       help: mov.movies,
       index: 1,
     };
-
-    this.handleChangesss = this.handleChangesss.bind(this);
-    this.handleChangess = this.handleChangess.bind(this);
-    this.handleChanges = this.handleChanges.bind(this);
   }
 
-  handleChangesss(event) {
+  handleChangeType = (event) => {
     const string = event.target.value;
     var p = [];
-    for(var i = 0; i < mov.movies.length; i++) {
+    mov.movies.forEach((item, index) => {
+      var obj = item;
+      if(obj === undefined) return;
+      if(string === obj.type) { p[index] = obj; }
+    });
+    /*for(var i = 0; i < mov.movies.length; i++) {
       var obj = mov.movies[i];
       if(string === obj.type) { p[i] = obj; }
-    }
+    }*/
     this.setState({ movies: p });
     this.setState({ help: p });
-}
+};
 
-  handleChanges(event) {
+  handleChangeRated = (event) => {
     var h = [];
     if(this.state.index === 2) h = this.state.movies;
     else h = this.state.help;
     const string = event.target.value;
     var p = [];
-    for(var i = 0; i < h.length; i++) {
+    h.forEach((item, index) => {
+      var obj = item;
+      if(obj === undefined) return;
+      if(string === obj.rated) { p[index] = obj; } 
+    });
+  /*for(var i = 0; i < h.length; i++) {
       var obj = h[i];
       if(obj === undefined) continue;
       if(string === obj.rated) { p[i] = obj; } 
-    }
+    }*/
       this.setState({ help: p });
       this.setState({ index: 2 });
-}
+};
 
-  handleChangess(event) {
+  handleChangeGenre = (event) => {
     var h = [];
     if(this.state.index === 1) h = this.state.movies;
     else h = this.state.help;
     const string = event.target.value;
     var p = [];
-    for(var i = 0; i < h.length; i++) {
+    h.forEach((item, index) => {
+      var obj = item;
+      if(obj === undefined) return;
+      obj.genre.forEach((items) => {
+        var sec = items;
+        if(sec === undefined) return;
+        if(string === sec) { p[index] = obj; }
+      });
+    });
+  /*for(var i = 0; i < h.length; i++) {
       var obj = h[i];
       if(obj === undefined) continue;
       for(var j = 0; j < obj.genre.length; j++) {
@@ -60,9 +75,9 @@ class App extends Component {
         if(sec === undefined) continue;
         if(string === sec) { p[i] = obj; } 
       }
-    }
+    }*/
       this.setState({ help: p });
-  }
+  };
 
 
 
@@ -70,93 +85,99 @@ class App extends Component {
     return (
       <div className="App-container">
       <div className="side">
+      <h6>(First select)</h6>
       <h3>Type</h3>
       <ul>
       <label className="container">Movies
-        <input type="radio" name="radio1" value={"movie"} onClick={this.handleChangesss}></input>
+        <input type="radio" name="radio1" value={"movie"} onClick={this.handleChangeType}></input>
         <span className="checkmark"></span>
       </label>
       <label className="container">Series
-        <input type="radio" name="radio1" value={"series"} onClick={this.handleChangesss}></input>
+        <input type="radio" name="radio1" value={"series"} onClick={this.handleChangeType}></input>
         <span className="checkmark"></span>
       </label>
       </ul>
+      <br></br>
+      <hr></hr>
+      <h6>(Then select)</h6>
       <h3>Rated</h3>
       <ul>
       <label className="container">G
-        <input type="radio" name="radio" value={"G"} onClick={this.handleChanges}></input>
+        <input type="radio" name="radio2" value={"G"} onClick={this.handleChangeRated}></input>
         <span className="checkmark"></span>
       </label>
       <label className="container">PG
-        <input type="radio" name="radio" value={"PG"} onClick={this.handleChanges}></input>
+        <input type="radio" name="radio2" value={"PG"} onClick={this.handleChangeRated}></input>
         <span className="checkmark"></span>
       </label>
       <label className="container">PG-13
-        <input type="radio" name="radio" value={"PG-13"} onClick={this.handleChanges}></input>
+        <input type="radio" name="radio2" value={"PG-13"} onClick={this.handleChangeRated}></input>
         <span className="checkmark"></span>
       </label>
       <label className="container">R
-        <input type="radio" name="radio" value={"R"} onClick={this.handleChanges}></input>
+        <input type="radio" name="radio2" value={"R"} onClick={this.handleChangeRated}></input>
         <span className="checkmark"></span>
       </label>
       <label className="container">NC-17
-        <input type="radio" name="radio" value={"NC-17"} onClick={this.handleChanges}></input>
+        <input type="radio" name="radio2" value={"NC-17"} onClick={this.handleChangeRated}></input>
         <span className="checkmark"></span>
       </label>
       <label className="container">N/A
-        <input type="radio" name="radio" value={"N/A"} onClick={this.handleChanges}></input>
+        <input type="radio" name="radio2" value={"N/A"} onClick={this.handleChangeRated}></input>
         <span className="checkmark"></span>
       </label>
         <label className="container">TV-14
-        <input type="radio" name="radio" value={"TV-14"} onClick={this.handleChanges}></input>
+        <input type="radio" name="radio2" value={"TV-14"} onClick={this.handleChangeRated}></input>
         <span className="checkmark"></span>
       </label>
       <label className="container">TV-MA
-        <input type="radio" name="radio" value={"TV-MA"} onClick={this.handleChanges}></input>
+        <input type="radio" name="radio2" value={"TV-MA"} onClick={this.handleChangeRated}></input>
         <span className="checkmark"></span>
       </label>
       <label className="container">TV-Y7
-        <input type="radio" name="radio" value={"TV-Y7"} onClick={this.handleChanges}></input>
+        <input type="radio" name="radio2" value={"TV-Y7"} onClick={this.handleChangeRated}></input>
         <span className="checkmark"></span>
       </label>
       </ul>
-      
+      <br></br>
+      <hr></hr>
+      <h6>(And, finally, select)</h6>
       <h3>Genre</h3>
       <ul>
       <label className="containers">Action
-        <input type="checkbox" value={"Action"} onClick={this.handleChangess}></input>
+        <input type="checkbox" value={"Action"} onClick={this.handleChangeGenre}></input>
         <span className="checkmarks"></span>
       </label>
       <label className="containers">Adventure
-        <input type="checkbox" value={"Adventure"} onClick={this.handleChangess}></input>
+        <input type="checkbox" value={"Adventure"} onClick={this.handleChangeGenre}></input>
         <span className="checkmarks"></span>
       </label>
       <label className="containers">Comedy
-      <input type="checkbox" value={"Comedy"} onClick={this.handleChangess}></input>
+      <input type="checkbox" value={"Comedy"} onClick={this.handleChangeGenre}></input>
         <span className="checkmarks"></span>
       </label>
       <label className="containers">Crime
-      <input type="checkbox" value={"Crime"} onClick={this.handleChangess}></input>
+      <input type="checkbox" value={"Crime"} onClick={this.handleChangeGenre}></input>
         <span className="checkmarks"></span>
       </label>
       <label className="containers">Drama
-        <input type="checkbox" value={"Drama"} onClick={this.handleChangess}></input>
+        <input type="checkbox" value={"Drama"} onClick={this.handleChangeGenre}></input>
         <span className="checkmarks"></span>
       </label>
       <label className="containers">Family
-        <input type="checkbox" value={"Family"} onClick={this.handleChangess}></input>
+        <input type="checkbox" value={"Family"} onClick={this.handleChangeGenre}></input>
         <span className="checkmarks"></span>
       </label>
       <label className="containers">Fantasy
-        <input type="checkbox" value={"Fantasy"} onClick={this.handleChangess}></input>
+        <input type="checkbox" value={"Fantasy"} onClick={this.handleChangeGenre}></input>
         <span className="checkmarks"></span>
       </label>
       <label className="containers">Horror
-        <input type="checkbox" value={"Horror"} onClick={this.handleChangess}></input>
+        <input type="checkbox" value={"Horror"} onClick={this.handleChangeGenre}></input>
         <span className="checkmarks"></span>
       </label>
       <label className="containers">Romance
-        <input type="checkbox" value={"Romance"} onClick={this.handleChangess}></input>
+        <input type="checkbox" value={"Romance"} onClick={this.handleChangeGenre}></input>
         <span className="checkmarks"></span>
       </label>
       </ul>
